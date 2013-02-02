7983d8f8337edf76ebd3b2987876237dcb577d6c
Ian Forsey <ian.forsey@net-a-porter.com>
2013-02-02 17:19:10 +0000

    Add \n\r characters to encoding test

--- src/test/scala/com/github/theon/urlutils/EncodingTests.scala
+++ src/test/scala/com/github/theon/urlutils/EncodingTests.scala
@@ -36,8 +36,8 @@ class EncodingTests extends FlatSpec with ShouldMatchers {
   }
 
   "Reserved characters" should "be percent encoded" in {
-    val uri = "http://theon.github.com/uris-in-scala.html" ? ("reserved" -> ":/?#[]@!$&'()*+,;={}\\")
-    uri.toString should equal ("http://theon.github.com/uris-in-scala.html?reserved=%3A%2F%3F%23%5B%5D%40%21%24%26%27%28%29%2A%2B%2C%3B%3D%7B%7D%5C")
+    val uri = "http://theon.github.com/uris-in-scala.html" ? ("reserved" -> ":/?#[]@!$&'()*+,;={}\\\n\r")
+    uri.toString should equal ("http://theon.github.com/uris-in-scala.html?reserved=%3A%2F%3F%23%5B%5D%40%21%24%26%27%28%29%2A%2B%2C%3B%3D%7B%7D%5C%0A%0D")
   }
 
   "Chinese characters" should "be percent encoded" in {

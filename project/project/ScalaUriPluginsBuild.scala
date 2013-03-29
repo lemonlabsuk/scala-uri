3cc0d3e94ce956c40793b3a74120047539064dc5
Ian Forsey <ian.forsey@net-a-porter.com>
2013-03-29 17:40:58 +0000

    Add plugins build file for easy compiling with local checkout of xsbt-coveralls-plugin sources

---
+++ project/project/ScalaUriPluginsBuild.scala
@@ -0,0 +1,20 @@
+//import sbt._
+//import sbt.Keys._
+//
+//object ScalaUriPluginsBuild extends Build {
+//
+//  /**
+//   * Used when testing changes to the xsbt-coveralls-plugin
+//   * Uncomment this, and comment out
+//   *
+//   * addSbtPlugin("com.github.theon" %% "xsbt-coveralls-plugin" % "0.0.1-SNAPSHOT")
+//   *
+//   * in scala-uri/project/build.sbt
+//   */
+//  lazy val scalaUriRoot = Project(
+//    id = "scala-uri",
+//    base = file(".")
+//  ) dependsOn(coverallsPlugin)
+//
+//  lazy val coverallsPlugin = file("../../xsbt-coveralls-plugin")
+//}

ed9a4629f6a7299e20703cc2b4b9099dac0b0eca
Ian Forsey <forsey@gmail.com>
2019-04-15 22:41:32 +0100

    Update the public suffix trie every release

--- publish.sh
+++ publish.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 
-sbt clean coverageOff "project scalaUriJVM" +publishSigned "project scalaUriJS" +publishSigned
+sbt updatePublicSuffixes
+git add jvm/src/main/resources/public_suffix_trie.json
+git commit -m"Update public suffixes"
+
+sbt test clean coverageOff "project scalaUriJVM" +publishSigned "project scalaUriJS" +publishSigned

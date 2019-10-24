1e8ec7fb00be9a23016c233e52c8633546c4a4e4
Ian Forsey <forsey@gmail.com>
2019-10-25 00:13:56 +0100

    Update script to publish new modules as well

--- publish.sh
+++ publish.sh
@@ -4,4 +4,5 @@ sbt updatePublicSuffixes
 git add jvm/src/main/resources/public_suffix_trie.json
 git commit -m"Update public suffixes"
 
-sbt +test clean coverageOff "project scalaUriJVM" +publishSigned "project scalaUriJS" +publishSigned
+sbt +test clean coverageOff +publishSigned
+

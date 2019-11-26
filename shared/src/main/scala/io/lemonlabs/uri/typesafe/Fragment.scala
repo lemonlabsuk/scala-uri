3e37fa0be230dd75b2ff6410ca90aa6511233c1c
Yuriy <stremlenye@gmail.com>
2019-11-26 17:12:24 +0100

    Adds Contravariant instances to the Fragment, PathPart, QueryValue, QueryKey, TraversableParams

--- shared/src/main/scala/io/lemonlabs/uri/typesafe/Fragment.scala
+++ shared/src/main/scala/io/lemonlabs/uri/typesafe/Fragment.scala
@@ -1,6 +1,9 @@
 package io.lemonlabs.uri.typesafe
 
+import cats.{Contravariant, Eq}
 import simulacrum.typeclass
+import cats.syntax.contravariant._
+
 import scala.language.implicitConversions
 
 @typeclass trait Fragment[A] {
@@ -9,11 +12,24 @@ import scala.language.implicitConversions
 
 object Fragment extends FragmentInstances
 
-trait FragmentInstances1 {
+sealed trait FragmentInstances2 {
+  implicit val contravariant: Contravariant[Fragment] = new Contravariant[Fragment] {
+    def contramap[A, B](fa: Fragment[A])(f: B => A): Fragment[B] = b => fa.fragment(f(b))
+  }
+}
+
+sealed trait FragmentInstances1 extends FragmentInstances2 {
   implicit val stringFragment: Fragment[String] = a => a
+  implicit final val booleanQueryValue: Fragment[Boolean] = stringFragment.contramap(_.toString)
+  implicit final val charQueryValue: Fragment[Char] = stringFragment.contramap(_.toString)
+  implicit final val intQueryValue: Fragment[Int] = stringFragment.contramap(_.toString)
+  implicit final val longQueryValue: Fragment[Long] = stringFragment.contramap(_.toString)
+  implicit final val floatQueryValue: Fragment[Float] = stringFragment.contramap(_.toString)
+  implicit final val doubleQueryValue: Fragment[Double] = stringFragment.contramap(_.toString)
+  implicit final val uuidQueryValue: Fragment[java.util.UUID] = stringFragment.contramap(_.toString)
   implicit val noneFragment: Fragment[None.type] = _ => ""
 }
 
-trait FragmentInstances extends FragmentInstances1 {
+sealed trait FragmentInstances extends FragmentInstances1 {
   implicit def optionFragment[A: Fragment]: Fragment[Option[A]] = _.map(Fragment[A].fragment).getOrElse("")
 }

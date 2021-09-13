4bdb2775bc38406cdc0a7e11c2b40b548d3b6515
Gregor Ihmor <gregor.ihmor@gmail.com>
2021-09-13 15:07:10 +0200

    fixed ambiguous method call to ===
    cats and scalatic both use the === operator, only the cats operator behaves correctly

--- shared/src/test/scala/io/lemonlabs/uri/CatsTests.scala
+++ shared/src/test/scala/io/lemonlabs/uri/CatsTests.scala
@@ -3,85 +3,81 @@ package io.lemonlabs.uri
 import org.scalatest.flatspec.AnyFlatSpec
 import cats.implicits._
 import org.scalatest.matchers.should.Matchers
-import cats.kernel.Comparison
+import cats.kernel.{Comparison, Eq}
 
 class CatsTests extends AnyFlatSpec with Matchers {
-  trait CatsTestCase {
-    val convertToEqualizer = () // shadow ScalaTest
-  }
-
-  "Eq" should "be supported for Uri" in new CatsTestCase {
+  "Eq" should "be supported for Uri" in {
     val uri = Uri.parse("https://typelevel.org/cats/")
     val uri2: Uri = AbsoluteUrl.parse("https://typelevel.org/cats/")
 
-    (uri === uri2) should equal(true)
+    (uri eqv uri2) should equal(true)
   }
 
-  it should "be supported for unordered query params Uri" in new CatsTestCase {
+  it should "be supported for unordered query params Uri" in {
     import Uri.unordered._
 
     val uri = Uri.parse("https://typelevel.org/cats/?a=1&b=two")
     val uri2: Uri = AbsoluteUrl.parse("https://typelevel.org/cats/?b=two&a=1")
 
-    (uri === uri2) should equal(true)
+    (uri eqv uri2) should equal(true)
   }
 
-  it should "be supported for Url" in new CatsTestCase {
+  it should "be supported for Url" in {
     val uri = Url.parse("https://typelevel.org/cats/")
     val uri2: Url = AbsoluteUrl.parse("https://typelevel.org/cats/")
 
-    (uri === uri2) should equal(true)
+    (uri eqv uri2) should equal(true)
   }
 
-  it should "be supported unordered query params Url" in new CatsTestCase {
+  it should "be supported unordered query params Url" in {
     import Url.unordered._
 
     val uri = Url.parse("https://typelevel.org/cats/?a=1&b=two")
     val uri2: Url = AbsoluteUrl.parse("https://typelevel.org/cats/?b=two&a=1")
 
-    (uri === uri2) should equal(true)
+    (uri eqv uri2) should equal(true)
   }
 
-  it should "be supported for RelativeUrl" in new CatsTestCase {
+  it should "be supported for RelativeUrl" in {
     val uri: Url = RelativeUrl.parse("/cats2/")
     val uri2: Url = RelativeUrl.parse("/cats/")
 
-    (uri === uri2) should equal(false)
+    (uri eqv uri2) should equal(false)
   }
 
-  it should "be supported for unordered query params RelativeUrl" in new CatsTestCase {
+  it should "be supported for unordered query params RelativeUrl" in {
     import RelativeUrl.unordered._
 
     val uri = RelativeUrl.parse("/cats2/?b=two&a=1")
     val uri2 = RelativeUrl.parse("/cats/?a=1&b=two")
 
-    (uri === uri2) should equal(false)
+    (uri eqv uri2) should equal(false)
   }
 
-  it should "be supported for UrlWithAuthority" in new CatsTestCase {
+  it should "be supported for UrlWithAuthority" in {
     val uri = UrlWithAuthority.parse("https://typelevel.org/cats/")
     val uri2: UrlWithAuthority = AbsoluteUrl.parse("https://typelevel.org/cats/")
 
-    (uri === uri2) should equal(true)
+    (uri eqv uri2) should equal(true)
   }
 
-  it should "be supported for unordered query params UrlWithAuthority" in new CatsTestCase {
+  it should "be supported for unordered query params UrlWithAuthority" in {
     import UrlWithAuthority.unordered._
 
     val uri = UrlWithAuthority.parse("https://typelevel.org/cats/?a=1&b=two")
     val uri2: UrlWithAuthority = AbsoluteUrl.parse("https://typelevel.org/cats/?b=two&a=1")
 
-    (uri === uri2) should equal(true)
+    (uri eqv uri2) should equal(true)
   }
 
-  it should "be supported for ProtocolRelativeUrl" in new CatsTestCase {
+  it should "be supported for ProtocolRelativeUrl" in {
     val uri = ProtocolRelativeUrl.parse("//typelevel.org/cats/")
     val uri2 = ProtocolRelativeUrl.parse("//typelevel.org/cats/?different=true")
 
     (uri =!= uri2) should equal(true)
   }
 
-  it should "be supported for unordered query params ProtocolRelativeUrl" in new CatsTestCase {
+  it should "be supported for unordered query params ProtocolRelativeUrl" in {
     import ProtocolRelativeUrl.unordered._
 
     val uri = ProtocolRelativeUrl.parse("//typelevel.org/cats/?b=two&a=1")
@@ -90,186 +86,186 @@ class CatsTests extends AnyFlatSpec with Matchers {
     (uri =!= uri2) should equal(false)
   }
 
-  it should "be supported for AbsoluteUrl" in new CatsTestCase {
+  it should "be supported for AbsoluteUrl" in {
     val uri = AbsoluteUrl.parse("https://typelevel.org/cats/")
     val uri2 = AbsoluteUrl.parse("https://typelevel.org/cats/")
 
-    (uri === uri2) should equal(true)
+    (uri eqv uri2) should equal(true)
   }
 
-  it should "be supported for unordered query params AbsoluteUrl" in new CatsTestCase {
+  it should "be supported for unordered query params AbsoluteUrl" in {
     import AbsoluteUrl.unordered._
 
     val uri = AbsoluteUrl.parse("https://typelevel.org/cats/?a=1&b=two")
     val uri2 = AbsoluteUrl.parse("https://typelevel.org/cats/?b=two&a=1")
 
-    (uri === uri2) should equal(true)
+    (uri eqv uri2) should equal(true)
   }
 
-  it should "be supported for UrlWithoutAuthority" in new CatsTestCase {
+  it should "be supported for UrlWithoutAuthority" in {
     val uri = UrlWithoutAuthority.parse("mailto:someone@somewhere.com")
     val uri2 = UrlWithoutAuthority.parse("mailto:someoneelse@somewhereelse.com")
 
-    (uri === uri2) should equal(false)
+    (uri eqv uri2) should equal(false)
   }
 
-  it should "be supported for unordered query params UrlWithoutAuthority" in new CatsTestCase {
+  it should "be supported for unordered query params UrlWithoutAuthority" in {
     import UrlWithoutAuthority.unordered._
 
     val uri = UrlWithoutAuthority.parse("mailto:someone@somewhere.com?b=two&a=1")
     val uri2 = UrlWithoutAuthority.parse("mailto:someoneelse@somewhereelse.com?a=1&b=two")
 
-    (uri === uri2) should equal(false)
+    (uri eqv uri2) should equal(false)
   }
 
-  it should "be supported for SimpleUrlWithoutAuthority" in new CatsTestCase {
+  it should "be supported for SimpleUrlWithoutAuthority" in {
     val uri = SimpleUrlWithoutAuthority.parse("mailto:someone@somewhere.com")
     val uri2 = SimpleUrlWithoutAuthority.parse("mailto:someone@somewhere.com")
 
-    (uri === uri2) should equal(true)
+    (uri eqv uri2) should equal(true)
   }
 
-  it should "be supported for unordered query params SimpleUrlWithoutAuthority" in new CatsTestCase {
+  it should "be supported for unordered query params SimpleUrlWithoutAuthority" in {
     import SimpleUrlWithoutAuthority.unordered._
 
     val uri = SimpleUrlWithoutAuthority.parse("mailto:someone@somewhere.com?a=1&b=two")
     val uri2 = SimpleUrlWithoutAuthority.parse("mailto:someone@somewhere.com?b=two&a=1")
 
-    (uri === uri2) should equal(true)
+    (uri eqv uri2) should equal(true)
   }
 
-  it should "be supported for DataUrl" in new CatsTestCase {
+  it should "be supported for DataUrl" in {
     val uri = DataUrl.parse("data:,A%20brief%20note")
     val uri2 = DataUrl.parse("data:,Another%20brief%20note")
 
     (uri =!= uri2) should equal(true)
   }
 
-  it should "be supported for ScpLikeUrl" in new CatsTestCase {
+  it should "be supported for ScpLikeUrl" in {
     val uri = ScpLikeUrl.parse("root@host:/root/file.tar.gz")
     val uri2 = ScpLikeUrl.parse("root@host:/root/file2.tar.gz")
 
     (uri =!= uri2) should equal(true)
   }
 
-  it should "be supported for Urn" in new CatsTestCase {
+  it should "be supported for Urn" in {
     val uri = Urn.parse("urn:cats:1")
     val uri2 = Urn.parse("urn:cats:2")
 
     (uri =!= uri2) should equal(true)
   }
 
-  it should "be supported for Authority" in new CatsTestCase {
+  it should "be supported for Authority" in {
     val authority = Authority.parse("typelevel.org:443")
     val authority2 = Authority.parse("typelevel.org:443")
 
-    (authority === authority2) should equal(true)
+    (authority eqv authority2) should equal(true)
   }
 
-  it should "be supported for UserInfo" in new CatsTestCase {
+  it should "be supported for UserInfo" in {
     val userInfo = UserInfo("user", "password")
     val userInfo2 = UserInfo("user", "password2")
 
     (userInfo =!= userInfo2) should equal(true)
   }
 
-  it should "be supported for Host" in new CatsTestCase {
+  it should "be supported for Host" in {
     val host = Host.parse("typelevel.org")
     val host2 = Host.parse("typelevel.org")
 
     (host =!= host2) should equal(false)
   }
 
-  it should "be supported for DomainName" in new CatsTestCase {
+  it should "be supported for DomainName" in {
     val host = DomainName.parse("typelevel.org")
     val host2 = DomainName.parse("www.typelevel.org")
 
     (host =!= host2) should equal(true)
   }
 
-  it should "be supported for Ipv4" in new CatsTestCase {
+  it should "be supported for Ipv4" in {
     val host = IpV4.parse("8.8.8.8")
     val host2 = IpV4.parse("8.8.8.8")
 
-    (host === host2) should equal(true)
+    (host eqv host2) should equal(true)
   }
 
-  it should "be supported for Ipv6" in new CatsTestCase {
+  it should "be supported for Ipv6" in {
     val host = IpV6.parse("[1f4:0:0:1e::1]")
     val host2 = IpV6.parse("[1f4:0:0:1e::1]")
 
-    (host === host2) should equal(true)
+    (host eqv host2) should equal(true)
   }
 
-  it should "be supported for MediaType" in new CatsTestCase {
+  it should "be supported for MediaType" in {
     val mediaType = MediaType("text/plain".some, Vector("charset" -> "utf8"))
     val mediaType2 = MediaType("text/plain".some, Vector.empty)
 
-    (mediaType === mediaType2) should equal(false)
+    (mediaType eqv mediaType2) should equal(false)
   }
 
-  it should "be supported for Path" in new CatsTestCase {
+  it should "be supported for Path" in {
     val path = Path.parse("/cats/")
     val path2 = Path.parse("/cats/")
 
-    (path === path2) should equal(true)
+    (path eqv path2) should equal(true)
   }
 
-  it should "be supported for UrlPath" in new CatsTestCase {
+  it should "be supported for UrlPath" in {
     val path = UrlPath.parse("/cats/")
     val path2 = UrlPath.parse("/cats/")
 
-    (path === path2) should equal(true)
+    (path eqv path2) should equal(true)
   }
 
-  it should "be supported for AbsoluteOrEmptyPath" in new CatsTestCase {
+  it should "be supported for AbsoluteOrEmptyPath" in {
     val path: AbsoluteOrEmptyPath = AbsolutePath.fromParts("cats")
     val path2: AbsoluteOrEmptyPath = AbsolutePath.fromParts("cats")
 
-    (path === path2) should equal(true)
+    (path eqv path2) should equal(true)
   }
 
-  it should "be supported for RootlessPath" in new CatsTestCase {
+  it should "be supported for RootlessPath" in {
     val path = RootlessPath.fromParts("cats")
     val path2 = RootlessPath.fromParts("cats2")
 
     (path =!= path2) should equal(true)
   }
 
-  it should "be supported for AbsolutePath" in new CatsTestCase {
+  it should "be supported for AbsolutePath" in {
     val path = AbsolutePath.fromParts("cats")
     val path2 = AbsolutePath.fromParts("cats")
 
-    (path === path2) should equal(true)
+    (path eqv path2) should equal(true)
   }
 
-  it should "be supported for UrnPath" in new CatsTestCase {
+  it should "be supported for UrnPath" in {
     val path = UrnPath.parse("cats:1")
     val path2 = UrnPath.parse("cats:2")
 
     (path =!= path2) should equal(true)
   }
 
-  it should "be supported for QueryString" in new CatsTestCase {
+  it should "be supported for QueryString" in {
     val qs = QueryString.parse("a=1&b=2")
     val qs2 = QueryString.parse("a=1&b=2")
 
-    (qs === qs2) should equal(true)
+    (qs eqv qs2) should equal(true)
   }
 
-  it should "be supported for unordered QueryString" in new CatsTestCase {
+  it should "be supported for unordered QueryString" in {
     val qs = QueryString.parse("a=1&b=2")
     val qs2 = QueryString.parse("b=2&a=1")
 
     import QueryString.unordered._
-    (qs === qs2) should equal(true)
+    (qs eqv qs2) should equal(true)
   }
 
-  it should "be supported for ordered QueryString" in new CatsTestCase {
+  it should "be supported for ordered QueryString" in {
     val qs = QueryString.parse("a=1&b=2")
     val qs2 = QueryString.parse("b=2&a=1")
 
-    (qs === qs2) should equal(false)
+    (qs eqv qs2) should equal(false)
   }
 
   "Show" should "be supported for Uri" in {
@@ -392,145 +388,145 @@ class CatsTests extends AnyFlatSpec with Matchers {
     qs.show should equal("a=1&b=2")
   }
 
-  "Order" should "be supported for Uri" in new CatsTestCase {
+  "Order" should "be supported for Uri" in {
     val uri = Uri.parse("https://typelevel.org/cats2/")
     val uri2: Uri = AbsoluteUrl.parse("https://typelevel.org/cats/")
     (uri comparison uri2) should equal(Comparison.GreaterThan)
   }
 
-  it should "be supported for Url" in new CatsTestCase {
+  it should "be supported for Url" in {
     val uri = Url.parse("https://typelevel.org/cats/")
     val uri2: Url = AbsoluteUrl.parse("https://typelevel.org/cats/")
     (uri comparison uri2) should equal(Comparison.EqualTo)
   }
 
-  it should "be supported for RelativeUrl" in new CatsTestCase {
+  it should "be supported for RelativeUrl" in {
     val uri: Url = RelativeUrl.parse("/cats2/")
     val uri2: Url = RelativeUrl.parse("/cats/")
     (uri comparison uri2) should equal(Comparison.GreaterThan)
   }
 
-  it should "be supported for UrlWithAuthority" in new CatsTestCase {
+  it should "be supported for UrlWithAuthority" in {
     val uri = UrlWithAuthority.parse("https://typelevel.org/cats/")
     val uri2: UrlWithAuthority = AbsoluteUrl.parse("https://typelevel.org/cats/")
     (uri comparison uri2) should equal(Comparison.EqualTo)
   }
 
-  it should "be supported for ProtocolRelativeUrl" in new CatsTestCase {
+  it should "be supported for ProtocolRelativeUrl" in {
     val uri = ProtocolRelativeUrl.parse("//typelevel.org/cats/")
     val uri2 = ProtocolRelativeUrl.parse("//typelevel.org/cats/?different=true")
     (uri comparison uri2) should equal(Comparison.LessThan)
   }
 
-  it should "be supported for AbsoluteUrl" in new CatsTestCase {
+  it should "be supported for AbsoluteUrl" in {
     val uri = AbsoluteUrl.parse("https://typelevel.org/cats/")
     val uri2 = AbsoluteUrl.parse("https://typelevel.org/cats/")
     (uri comparison uri2) should equal(Comparison.EqualTo)
   }
 
-  it should "be supported for UrlWithoutAuthority" in new CatsTestCase {
+  it should "be supported for UrlWithoutAuthority" in {
     val uri = UrlWithoutAuthority.parse("mailto:someone@somewhere.com")
     val uri2 = UrlWithoutAuthority.parse("mailto:someoneelse@somewhereelse.com")
     (uri comparison uri2) should equal(Comparison.LessThan)
   }
 
-  it should "be supported for SimpleUrlWithoutAuthority" in new CatsTestCase {
+  it should "be supported for SimpleUrlWithoutAuthority" in {
     val uri = SimpleUrlWithoutAuthority.parse("mailto:someone@somewhere.com")
     val uri2 = SimpleUrlWithoutAuthority.parse("mailto:someone@somewhere.com")
     (uri comparison uri2) should equal(Comparison.EqualTo)
   }
 
-  it should "be supported for DataUrl" in new CatsTestCase {
+  it should "be supported for DataUrl" in {
     val uri = DataUrl.parse("data:text;,A%20brief%20note")
     val uri2 = DataUrl.parse("data:text;base64,R0lGODdh")
     (uri comparison uri2) should equal(Comparison.LessThan)
   }
 
-  "Order" should "be supported for Urn" in new CatsTestCase {
+  "Order" should "be supported for Urn" in {
     val uri = Urn.parse("urn:cats:1")
     val uri2 = Urn.parse("urn:cats:2")
     (uri comparison uri2) should equal(Comparison.LessThan)
   }
 
-  it should "be supported for Authority" in new CatsTestCase {
+  it should "be supported for Authority" in {
     val authority = Authority.parse("typelevel.org:443")
     val authority2 = Authority.parse("typelevel.org:443")
     (authority comparison authority2) should equal(Comparison.EqualTo)
   }
 
-  it should "be supported for UserInfo" in new CatsTestCase {
+  it should "be supported for UserInfo" in {
     val userInfo = UserInfo("user", "password")
     val userInfo2 = UserInfo("user", "password2")
     (userInfo comparison userInfo2) should equal(Comparison.LessThan)
   }
 
-  it should "be supported for Host" in new CatsTestCase {
+  it should "be supported for Host" in {
     val host = Host.parse("typelevel.org")
     val host2 = Host.parse("typelevel.org")
     (host comparison host2) should equal(Comparison.EqualTo)
   }
 
-  it should "be supported for DomainName" in new CatsTestCase {
+  it should "be supported for DomainName" in {
     val host = DomainName.parse("typelevel.org")
     val host2 = DomainName.parse("www.typelevel.org")
     (host comparison host2) should equal(Comparison.LessThan)
   }
 
-  it should "be supported for Ipv4" in new CatsTestCase {
+  it should "be supported for Ipv4" in {
     val host = IpV4.parse("8.8.8.8")
     val host2 = IpV4.parse("8.8.8.8")
     (host comparison host2) should equal(Comparison.EqualTo)
   }
 
-  it should "be supported for Ipv6" in new CatsTestCase {
+  it should "be supported for Ipv6" in {
     val host = IpV6.parse("[1f4:0:0:1e::1]")
     val host2 = IpV6.parse("[1f4:0:0:1e::1]")
     (host comparison host2) should equal(Comparison.EqualTo)
   }
 
-  it should "be supported for MediaType" in new CatsTestCase {
+  it should "be supported for MediaType" in {
     val mediaType = MediaType("text/plain".some, Vector("charset" -> "utf8"))
     val mediaType2 = MediaType("text/plain".some, Vector.empty)
     (mediaType comparison mediaType2) should equal(Comparison.GreaterThan)
   }
 
-  it should "be supported for Path" in new CatsTestCase {
+  it should "be supported for Path" in {
     val path = Path.parse("/cats/")
     val path2 = Path.parse("/cats/")
     (path comparison path2) should equal(Comparison.EqualTo)
   }
 
-  it should "be supported for UrlPath" in new CatsTestCase {
+  it should "be supported for UrlPath" in {
     val path = UrlPath.parse("/cats/")
     val path2 = UrlPath.parse("/cats/")
     (path comparison path2) should equal(Comparison.EqualTo)
   }
 
-  it should "be supported for AbsoluteOrEmptyPath" in new CatsTestCase {
+  it should "be supported for AbsoluteOrEmptyPath" in {
     val path: AbsoluteOrEmptyPath = AbsolutePath.fromParts("cats")
     val path2: AbsoluteOrEmptyPath = AbsolutePath.fromParts("cats")
     (path comparison path2) should equal(Comparison.EqualTo)
   }
 
-  it should "be supported for RootlessPath" in new CatsTestCase {
+  it should "be supported for RootlessPath" in {
     val path = RootlessPath.fromParts("cats")
     val path2 = RootlessPath.fromParts("cats2")
     (path comparison path2) should equal(Comparison.LessThan)
   }
 
-  it should "be supported for AbsolutePath" in new CatsTestCase {
+  it should "be supported for AbsolutePath" in {
     val path = AbsolutePath.fromParts("cats")
     val path2 = AbsolutePath.fromParts("cats")
     (path comparison path2) should equal(Comparison.EqualTo)
   }
 
-  it should "be supported for UrnPath" in new CatsTestCase {
+  it should "be supported for UrnPath" in {
     val path = UrnPath.parse("cats:1")
     val path2 = UrnPath.parse("cats:2")
     (path comparison path2) should equal(Comparison.LessThan)
   }
 
-  it should "be supported for QueryString" in new CatsTestCase {
+  it should "be supported for QueryString" in {
     val qs = QueryString.parse("a=1&b=2")
     val qs2 = QueryString.parse("a=1&b=2")
     (qs comparison qs2) should equal(Comparison.EqualTo)

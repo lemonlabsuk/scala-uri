6588d8177cb4fbca2f44784a9400ba861000a6e2
Gregor Ihmor <gregor.ihmor@gmail.com>
2021-09-14 15:44:52 +0200

    upgraded and restored scalafmt

--- shared/src/main/scala-2/io/lemonlabs/uri/typesafe/TraversablePathPartsDeriving.scala
+++ shared/src/main/scala-2/io/lemonlabs/uri/typesafe/TraversablePathPartsDeriving.scala
@@ -6,24 +6,24 @@ import shapeless.labelled._
 
 trait TraversablePathPartsDeriving {
   implicit def field[K <: Symbol, V](implicit
-                                     K: Witness.Aux[K],
-                                     V: PathPart[V]
-                                    ): TraversablePathParts[FieldType[K, V]] =
+      K: Witness.Aux[K],
+      V: PathPart[V]
+  ): TraversablePathParts[FieldType[K, V]] =
     (a: FieldType[K, V]) => V.splitPath(a)
 
   implicit def sub[K <: Symbol, V](implicit
-                                   K: Witness.Aux[K],
-                                   V: TraversablePathParts[V]
-                                  ): TraversablePathParts[FieldType[K, V]] =
+      K: Witness.Aux[K],
+      V: TraversablePathParts[V]
+  ): TraversablePathParts[FieldType[K, V]] =
     (a: FieldType[K, V]) => V.toSeq(a)
 
   implicit val hnil: TraversablePathParts[HNil] =
     (_: HNil) => Seq.empty
 
   implicit def hcons[H, T <: HList](implicit
-                                    H: TraversablePathParts[H],
-                                    T: TraversablePathParts[T]
-                                   ): TraversablePathParts[H :: T] =
+      H: TraversablePathParts[H],
+      T: TraversablePathParts[T]
+  ): TraversablePathParts[H :: T] =
     (a: H :: T) => H.toSeq(a.head) ++ T.toSeq(a.tail)
 
   def product[A, R <: HList](implicit gen: Generic.Aux[A, R], R: TraversablePathParts[R]): TraversablePathParts[A] =

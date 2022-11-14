5978d15a63bb1411d11b39b108e5cc8caf4c2ce4
Ian Forsey <forsey@gmail.com>
2022-11-14 21:23:47 +0000

    Remove runtime exceptions from tests

--- shared/src/test/scala/io/lemonlabs/uri/PatternMatchingTests.scala
+++ shared/src/test/scala/io/lemonlabs/uri/PatternMatchingTests.scala
@@ -1,96 +1,106 @@
 package io.lemonlabs.uri
 
+import org.scalatest.Inside
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class PatternMatchingTests extends AnyFlatSpec with Matchers {
-  "Uri.unapply" should "extract the path" in {
-    val Uri(path) = Urn.parse("urn:nid:nss")
-    path should equal(UrnPath("nid", "nss"))
+class PatternMatchingTests extends AnyFlatSpec with Matchers with Inside {
 
-    val Uri(path2) = Url.parse("http://localhost/my/lovely/path")
-    path2 should equal(AbsolutePath.fromParts("my", "lovely", "path"))
+  "Uri.unapply" should "extract the path" in {
+    inside(Urn.parse("urn:nid:nss")) { case Uri(path) =>
+      path should equal(UrnPath("nid", "nss"))
+    }
+
+    inside(Url.parse("http://localhost/my/lovely/path")) { case Uri(path2) =>
+      path2 should equal(AbsolutePath.fromParts("my", "lovely", "path"))
+    }
   }
 
   "Url.unapply" should "extract the path, query and fragment" in {
-    val Url(path, query, fragment) = Url.parse("/test?query=string#frag")
-    path should equal(AbsolutePath.fromParts("test"))
-    query should equal(QueryString.fromPairs("query" -> "string"))
-    fragment should equal(Some("frag"))
+    inside(Url.parse("/test?query=string#frag")) { case Url(path, query, fragment) =>
+      path should equal(AbsolutePath.fromParts("test"))
+      query should equal(QueryString.fromPairs("query" -> "string"))
+      fragment should equal(Some("frag"))
+    }
   }
 
   "RelativeUrl.unapply" should "extract the path, query and fragment" in {
-    val RelativeUrl(path, query, fragment) = Url.parse("/test?query=string#frag")
-    path should equal(AbsolutePath.fromParts("test"))
-    query should equal(QueryString.fromPairs("query" -> "string"))
-    fragment should equal(Some("frag"))
+    inside(Url.parse("/test?query=string#frag")) { case RelativeUrl(path, query, fragment) =>
+      path should equal(AbsolutePath.fromParts("test"))
+      query should equal(QueryString.fromPairs("query" -> "string"))
+      fragment should equal(Some("frag"))
+    }
   }
 
   "UrlWithAuthority.unapply" should "extract the authority, path, query and fragment" in {
-    val UrlWithAuthority(authority, path, query, fragment) =
-      Url.parse("http://localhost/test?query=string#frag")
-
-    authority.host should equal(DomainName("localhost"))
-    path should equal(AbsolutePath.fromParts("test"))
-    query should equal(QueryString.fromPairs("query" -> "string"))
-    fragment should equal(Some("frag"))
+    inside(Url.parse("http://localhost/test?query=string#frag")) {
+      case UrlWithAuthority(authority, path, query, fragment) =>
+        authority.host should equal(DomainName("localhost"))
+        path should equal(AbsolutePath.fromParts("test"))
+        query should equal(QueryString.fromPairs("query" -> "string"))
+        fragment should equal(Some("frag"))
+    }
   }
 
   "Host.unapply" should "extract the authority, path, query and fragment" in {
-    val Host(domain) = DomainName("abc")
-    domain should equal("abc")
+    inside(DomainName("abc")) { case Host(domain) =>
+      domain should equal("abc")
+    }
 
-    val Host(ipv4) = IpV4.parse("1.2.3.4")
-    ipv4 should equal("1.2.3.4")
+    inside(IpV4.parse("1.2.3.4")) { case Host(ipv4) =>
+      ipv4 should equal("1.2.3.4")
+    }
 
-    val Host(ipv6) = IpV6.parse("[1:2:3:4:5:6:7:8]")
-    ipv6 should equal("[1:2:3:4:5:6:7:8]")
+    inside(IpV6.parse("[1:2:3:4:5:6:7:8]")) { case Host(ipv6) =>
+      ipv6 should equal("[1:2:3:4:5:6:7:8]")
+    }
   }
 
   "PathParts.unapply()" should "match empty path and single slash only" in {
     // Should all run without throwing exception
-    val PathParts() = RootlessPath(Vector.empty)
-    val PathParts() = AbsolutePath(Vector.empty)
-    val PathParts() = EmptyPath
+    RootlessPath(Vector.empty) should matchPattern { case PathParts() => }
+    AbsolutePath(Vector.empty) should matchPattern { case PathParts() => }
+    EmptyPath should matchPattern { case PathParts() => }
 
     // Should not match
-    a[MatchError] should be thrownBy { val PathParts() = RootlessPath.fromParts("a") }
-    a[MatchError] should be thrownBy { val PathParts() = AbsolutePath.fromParts("a") }
+    RootlessPath.fromParts("a") shouldNot matchPattern { case PathParts() => }
+    AbsolutePath.fromParts("a") shouldNot matchPattern { case PathParts() => }
   }
 
   "PathParts.unapply(\"a\", \"b\")" should "match '/a/b' and 'a/b' only" in {
     // Should all run without throwing exception
-    val PathParts("a", "b") = RootlessPath.fromParts("a", "b")
-    val PathParts("a", "b") = AbsolutePath.fromParts("a", "b")
+    RootlessPath.fromParts("a", "b") should matchPattern { case PathParts("a", "b") => }
+    AbsolutePath.fromParts("a", "b") should matchPattern { case PathParts("a", "b") => }
 
     // Should not match
-    a[MatchError] should be thrownBy { val PathParts("a", "b") = RootlessPath.fromParts("a") }
-    a[MatchError] should be thrownBy { val PathParts("a", "b") = AbsolutePath.fromParts("a") }
+    RootlessPath.fromParts("a") shouldNot matchPattern { case PathParts("a", "b") => }
+    AbsolutePath.fromParts("a") shouldNot matchPattern { case PathParts("a", "b") => }
   }
 
   "PathParts.unapply(\"a\", _*)" should "match '/a.*' and 'a.*' only" in {
     // Should all run without throwing exception
-    val PathParts("a", _*) = RootlessPath.fromParts("a")
-    val PathParts("a", _*) = AbsolutePath.fromParts("a")
-    val PathParts("a", _*) = RootlessPath.fromParts("a", "b")
-    val PathParts("a", _*) = AbsolutePath.fromParts("a", "b")
+    RootlessPath.fromParts("a") should matchPattern { case PathParts("a", _*) => }
+    AbsolutePath.fromParts("a") should matchPattern { case PathParts("a", _*) => }
+    RootlessPath.fromParts("a", "b") should matchPattern { case PathParts("a", _*) => }
+    AbsolutePath.fromParts("a", "b") should matchPattern { case PathParts("a", _*) => }
 
     // Should not match
-    a[MatchError] should be thrownBy { val PathParts("a", _*) = RootlessPath.fromParts("x", "y", "z") }
-    a[MatchError] should be thrownBy { val PathParts("a", _*) = AbsolutePath.fromParts("x", "y", "z") }
+    RootlessPath.fromParts("x", "y", "z") shouldNot matchPattern { case PathParts("a", _*) => }
+    AbsolutePath.fromParts("x", "y", "z") shouldNot matchPattern { case PathParts("a", _*) => }
   }
 
   "EmptyPath.unapply()" should "match empty paths only, but not single slash" in {
     // Should all run without throwing exception
-    val EmptyPath() = EmptyPath
-    val EmptyPath() = RootlessPath(Vector.empty)
+    EmptyPath should matchPattern { case EmptyPath() => }
+    RootlessPath(Vector.empty) should matchPattern { case EmptyPath() => }
 
     // Should not match
-    a[MatchError] should be thrownBy { val EmptyPath() = AbsolutePath(Vector.empty) }
+    AbsolutePath(Vector.empty) shouldNot matchPattern { case EmptyPath() => }
   }
 
   "Path.unapply()" should "match any path" in {
-    val Path(parts) = AbsolutePath.fromParts("a", "b", "c")
-    parts should equal(Vector("a", "b", "c"))
+    inside(AbsolutePath.fromParts("a", "b", "c")) { case Path(parts) =>
+      parts should equal(Vector("a", "b", "c"))
+    }
   }
 }

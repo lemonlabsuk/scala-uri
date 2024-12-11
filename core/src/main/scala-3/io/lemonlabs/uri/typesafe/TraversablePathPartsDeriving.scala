b65b865af98820541f0758258c98fd01d0402bbb
Anton Sviridov <keynmol@gmail.com>
2024-12-11 09:53:36 +0000

    Init fork

--- core/src/main/scala-3/io/lemonlabs/uri/typesafe/TraversablePathPartsDeriving.scala
+++ core/src/main/scala-3/io/lemonlabs/uri/typesafe/TraversablePathPartsDeriving.scala
@@ -3,18 +3,20 @@ package io.lemonlabs.uri.typesafe
 import scala.compiletime.{erasedValue, summonInline}
 import scala.deriving.Mirror
 
+private class TraversablePathPartsDerived[A](elemInstances: List[TraversablePathParts[_]])
+    extends TraversablePathParts[A]:
+  override def toSeq(a: A): Seq[String] =
+    a.asInstanceOf[Product]
+      .productIterator
+      .zip(elemInstances)
+      .flatMap { case (field, tc) => tc.asInstanceOf[TraversablePathParts[Any]].toSeq(field) }
+      .toSeq
+
 trait TraversablePathPartsDeriving {
   inline def product[A](implicit m: Mirror.ProductOf[A]): TraversablePathParts[A] = {
     val elemInstances = summonAll[m.MirroredElemTypes]
 
-    new TraversablePathParts[A] {
-      override def toSeq(a: A): Seq[String] =
-        a.asInstanceOf[Product]
-          .productIterator
-          .zip(elemInstances)
-          .flatMap { case (field, tc) => tc.asInstanceOf[TraversablePathParts[Any]].toSeq(field) }
-          .toSeq
-    }
+    TraversablePathPartsDerived(elemInstances)
   }
 
   inline def summonAll[T <: Tuple]: List[TraversablePathParts[_]] =
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,15 +1,17 @@
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.0.6")
 
-addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject" % "1.3.1")
+addSbtPlugin("com.eed3si9n"       % "sbt-projectmatrix"             % "0.10.0")
 
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.12.0")
+addSbtPlugin("org.scala-js"     % "sbt-scalajs"      % "1.17.0")
 
-addSbtPlugin("com.github.sbt" % "sbt-pgp" % "2.2.1")
+addSbtPlugin("org.scala-native" % "sbt-scala-native" % "0.5.5")
 
-addSbtPlugin("com.typesafe" % "sbt-mima-plugin" % "1.1.3")
+addSbtPlugin("com.github.sbt"   % "sbt-ci-release"   % "1.9.0")
+
+addSbtPlugin("com.typesafe" % "sbt-mima-plugin" % "1.1.4")
 
 addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.5.2")
 
-addSbtPlugin("org.scalameta" % "sbt-mdoc" % "2.3.8")
+addSbtPlugin("org.scalameta" % "sbt-mdoc" % "2.6.1")
 
-addSbtPlugin("ch.epfl.scala" % "sbt-scalafix" % "0.11.1")
+addSbtPlugin("ch.epfl.scala" % "sbt-scalafix" % "0.13.0")
